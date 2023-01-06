615fb87de857a8e0ae82f42e181687a5212b30da
manylovv <yellowmanylov@gmail.com>
2023-01-06 21:40:30 +0700

    change button story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from '../components/Button';
+import { ThemeProvider } from '../theme/ThemeProvider';
+import defaultTheme from '../theme/defaultTheme';
 
 export default {
   title: 'Example/Button',
   component: Button,
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={defaultTheme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;

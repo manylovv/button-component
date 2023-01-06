4b85314e15989139b8dd8040c2d9e8b513e68464
manylovv <yellowmanylov@gmail.com>
2023-01-06 22:34:39 +0700

    add export of default theme

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Button } from './components/Button';
 import useTheme from './hooks/useTheme';
 import { ThemeProvider } from './theme/ThemeProvider';
 import { createTheme } from './theme/createTheme';
+import defaultTheme from './theme/defaultTheme';
 import type { Theme } from './theme/types';
 
-export { Button, ThemeProvider, useTheme, Theme, createTheme };
+export { Button, ThemeProvider, useTheme, Theme, defaultTheme, createTheme };

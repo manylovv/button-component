cfd58ae2b11a5aa180bdcd21ee9cec7d8ea687cb
manylovv <yellowmanylov@gmail.com>
2023-01-06 22:14:31 +0700

    export theme type and publish new version

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Button } from './components/Button';
 import { ThemeProvider } from './theme/ThemeProvider';
 import useTheme from './hooks/useTheme';
 import defaultTheme from './theme/defaultTheme';
+import type { Theme } from './theme/types';
 
-export { Button, ThemeProvider, useTheme, defaultTheme };
+export { Button, ThemeProvider, useTheme, Theme, defaultTheme };
